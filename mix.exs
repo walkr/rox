ae0521326d9b72eb47bfc7518fae59acf992c907
walkr <walkr@local>
2018-06-25 20:17:30 -0700

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rox.Mixfile do
   def project do
     [
       app: :rox,
-      version: "2.2.1",
+      version: "2.3.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
